f1ee515500251a5e98aee5427e06fec0dcb11623
NishargShah <nishargshah3101@gmail.com>
2020-04-02 02:34:28 +0530

    drop feature added

--- public/js/main.js
+++ public/js/main.js
@@ -3,13 +3,29 @@ import "@babel/polyfill";
 document.querySelector('#uploadForm').addEventListener('change', e => {
     console.log(e);
     const target = e.target;
-    e.path[2].classList.add('file_upload--started');
+    document.querySelector('.box_wrapper').classList.add('file_upload--started');
     const files = target.files;
     console.log(files);
 });
 
-document.querySelector('.close_arrow').addEventListener('click', e => {
-    // console.log(e);
-    // const target = e.target;
-    e.path[2].classList.remove('file_upload--started');
+// WHEN FILE COMES ( BOTH NEEDED TO PREVENT )
+window.addEventListener('dragover', e => e.preventDefault());
+
+// WHEN FILE COMES ( BOTH NEEDED TO PREVENT )
+window.addEventListener('dragenter', e => {
+    e.preventDefault();
+    document.querySelector('.container-fluid').classList.add('drag_fired');
+});
+
+// DROP
+window.addEventListener('drop', e => {
+    e.preventDefault();
+    document.querySelector('.container-fluid').classList.remove('drag_fired');
+    document.querySelector('.box_wrapper').classList.add('file_upload--started');
+    console.log(e);
+});
+
+// CLOSE ICON
+document.querySelector('.close_arrow').addEventListener('click', () => {
+    document.querySelector('.box_wrapper').classList.remove('file_upload--started');
 });

f96579a6272cbfcfc29e7d1321dd38d198abd3d3
NishargShah <nishargshah3101@gmail.com>
2020-04-02 00:35:37 +0530

    after upload frontend started

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,7 @@
 import "@babel/polyfill";
+
+document.querySelector('#uploadForm').addEventListener('change', e => {
+    console.log(e);
+    const target = e.target;
+    e.path[3].classList.add('file_upload--started');
+});
